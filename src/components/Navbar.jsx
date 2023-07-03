2e0a040c7cdc07a15a62793819917f8493a3aada
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-03 11:35:30 +0300

    add: sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <span className="logo">DPK Chat</span>
+      <div className="user">
+        <img
+          src="https://images.pexels.com/photos/17327920/pexels-photo-17327920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <span>Camander</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
