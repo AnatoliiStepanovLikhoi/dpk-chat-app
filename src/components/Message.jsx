85ae3bdc2c8d05053e1bd813be7dceabd95ad1e8
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-04 22:50:28 +0300

    add: mixins and message positioning

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,6 @@
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img
           src="https://images.pexels.com/photos/17327920/pexels-photo-17327920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -10,10 +10,10 @@ const Message = () => {
       </div>
       <div className="messageContent">
         <p>Hello world!</p>
-        {/* <img
+        <img
           src="https://images.pexels.com/photos/17327920/pexels-photo-17327920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Avatar"
-        /> */}
+        />
       </div>
     </div>
   );
