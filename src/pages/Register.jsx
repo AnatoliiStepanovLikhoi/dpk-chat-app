c66ecb067241db7c0f390bf8711e467082fa2d7a
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-05 22:07:44 +0300

    fix: refactor register on single responsibilities

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,11 @@
 import addAvatar from "../assets/addAvatar.png";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { auth, storage, db } from "../utils/firebase";
 import { useState } from "react";
-import { doc, setDoc } from "firebase/firestore";
+import handleSubmit from "../utils/registerUtils";
 
 const Register = () => {
   const [error, setError] = useState(false);
-  const handleSubmit = async (e) => {
+
+  const handleFormSubmit = (e) => {
     e.preventDefault();
 
     const displayName = e.target[0].value;
@@ -15,54 +13,7 @@ const Register = () => {
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-
-      const storageRef = ref(storage, displayName);
-
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          setError(true);
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateProfile(response.user, {
-              displayName,
-              photoURL: downloadURL,
-            });
-            await setDoc(doc(db, "users", response.user.uid), {
-              uid: response.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
-          });
-        }
-      );
-    } catch (error) {
-      setError(true);
-    }
+    handleSubmit(displayName, email, password, file, setError);
   };
 
   return (
@@ -70,7 +21,7 @@ const Register = () => {
       <div className="formWrapper">
         <span className="logo">DPK Chat</span>
         <span className="title">Register</span>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleFormSubmit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
---
+++ src/utils/registerUtil.js
@@ -0,0 +1,75 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { auth, storage, db } from "../utils/firebase";
+import { doc, setDoc } from "firebase/firestore";
+
+const createUser = async (displayName, email, password) => {
+  const response = await createUserWithEmailAndPassword(auth, email, password);
+  return response.user;
+};
+
+const uploadAvatar = (file, displayName) => {
+  return new Promise((resolve, reject) => {
+    const storageRef = ref(storage, displayName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL) => {
+            resolve(downloadURL);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      }
+    );
+  });
+};
+
+const updateUserProfile = (user, displayName, downloadURL) => {
+  return updateProfile(user, {
+    displayName,
+    photoURL: downloadURL,
+  });
+};
+
+const saveUserToDatabase = (user, displayName, email, downloadURL) => {
+  const userData = {
+    uid: user.uid,
+    displayName,
+    email,
+    photoURL: downloadURL,
+  };
+  return setDoc(doc(db, "users", user.uid), userData);
+};
+
+const handleSubmit = async (displayName, email, password, file, setError) => {
+  try {
+    const user = await createUser(displayName, email, password);
+    const downloadURL = await uploadAvatar(file, displayName);
+    await updateUserProfile(user, displayName, downloadURL);
+    await saveUserToDatabase(user, displayName, email, downloadURL);
+  } catch (error) {
+    setError(true);
+  }
+};
+
+export default handleSubmit;
